29daa5418a800724755ce39019fc25b775c660b4
rrodriguess20 <rodriguess.rebeca20@gmail.com>
2025-04-22 23:04:47 -0300

    Questão implementada

--- implementacao.js
+++ implementacao.js
@@ -21,3 +21,66 @@ funcionarios = [
 	{id: 8, nome: "Zapp Brannigan", funcao: empregos.NaoInformado},
 	{id: 6, nome: "Turanga Leela", funcao: empregos.Capitao},
 ];
+function ParticionarFuncionarios(array, left, right){
+
+	const pivo = array[Math.floor((left+right)/2)].funcao.ordem;
+
+	let i = left;
+	let j = right;
+
+	while(i<=j){
+
+		do{
+
+			i++;
+		}while(array[i].funcao.ordem<pivo);
+
+		do {
+
+			j--;
+		} while (array[j].funcao.ordem > pivo);
+
+		if(i<=j){
+
+			[array[i], array[j]] = [array[j], array[i]]; //troca de posicao
+
+			i++;
+			j--;
+		}
+
+	}
+
+	return i;
+}
+
+
+function QuickSort(array, left = 0, right = array.length - 1) {
+
+	if (left < right) {
+	  const index = ParticionarFuncionarios(array, left - 1, right + 1);
+  
+	  QuickSort(array, left, index - 1);
+	  QuickSort(array, index, right);
+	}
+  
+	return array;
+}
+
+function ImprimirFuncionarios(array){
+
+	console.log(`\nLista final ordenada:\n`);
+
+	array.forEach(f => {
+		
+		console.log(`${f.nome} - ${f.funcao.descricao} (ordem ${f.funcao.ordem})`);
+	});
+}
+
+
+function main(){
+
+	QuickSort(funcionarios);
+	ImprimirFuncionarios(funcionarios);
+}
+
+main();
